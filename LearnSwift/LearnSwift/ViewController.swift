fe9fc3b4db13850650e6ea0eed91f09fe244ca2b
Robin.Li <Robin.li@connext.com.cn>
2018-12-05 17:16:55 +0800

    update 文件

--- LearnSwift/LearnSwift/ViewController.swift
+++ LearnSwift/LearnSwift/ViewController.swift
@@ -8,13 +8,32 @@
 
 import UIKit
 
+struct Once {
+    static let identifier = "cell"
+    static var isRegister : Bool = false
+}
+
 class ViewController: UIViewController {
 
+     var tableView: UITableView!
+     var items:NSMutableArray?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView = UITableView(CGRectMake(0, 64,self.view.frame.size.width, self.view.frame.size.height))
+        tableView.dataSource = self as! UITableViewDataSource
+        tableView.delegate = self as! UITableViewDelegate
+        tableView.backgroundColor = UIColor.grayColor
+        self.view.addSubview(tableView)
+        tableView.registerClass(MyTableViewCell.classForCoder(), forCellReuseIdentifier: "cell")
+        self.automaticallyAdjustsScrollViewInsets = false
+        
+        
+       
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
